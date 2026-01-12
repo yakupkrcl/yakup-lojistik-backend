f55560abffc3c700e97b7c133a31a8f0a13cba18
yakupkrcl <151029434+yakupkrcl@users.noreply.github.com>
2026-01-12 14:39:38 +0300

    Global Exception Handler eklendi, 500 hatasi cozuldu

--- src/pages/shipper/CreateLoad.jsx
+++ src/pages/shipper/CreateLoad.jsx
@@ -147,10 +147,8 @@ const CreateLoad = () => {
               <select name="kalkisAdresiId" value={formData.kalkisAdresiId} onChange={handleChange} required>
                 <option value="">Seçiniz...</option>
                 {locations.map(loc => (
-<option key={loc.id} value={loc.id}>
-   {/* Başına bir emoji koy, bakalım mobilde görünecek mi */}
-   📍 {loc.sehir} - {loc.ilce}
-</option>                ))}
+                  <option key={loc.id} value={loc.id}>{loc.sehir} - {loc.ilce}</option>
+                ))}
               </select>
             </div>
 
@@ -159,10 +157,8 @@ const CreateLoad = () => {
               <select name="varisAdresiId" value={formData.varisAdresiId} onChange={handleChange} required>
                 <option value="">Seçiniz...</option>
                 {locations.map(loc => (
-<option key={loc.id} value={loc.id}>
-   {/* Başına bir emoji koy, bakalım mobilde görünecek mi */}
-   📍 {loc.sehir} - {loc.ilce}
-</option>                ))}
+                  <option key={loc.id} value={loc.id}>📍{loc.sehir} - {loc.ilce}</option>
+                ))}
               </select>
             </div>
 
